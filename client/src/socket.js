9131ad92d1047862f7317e452edd511edb482181
cozimus <cosimassari@gmail.com>
2023-03-30 22:40:40 +0200

    fixed server url on glitch

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "http://wist-online-server.glitch.me"
+    ? "https://wist-online-server.glitch.me"
     : // : "http://localhost:4000";
-      "http://wist-online-server.glitch.me";
+      "https://wist-online-server.glitch.me";
 
 export const socket = io(URL);
